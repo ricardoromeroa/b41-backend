3c276363379d1e8844bf9a0cfbc20aad9b130926
Ricardor <ricardoromeroavila@gmail.com>
2020-06-16 18:33:23 -0700

    add cors

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable linebreak-style */
 const express = require('express');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 
 const api = express();
 
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ extended: true }));
+api.use(cors());
 
 api.get('/', (req, res) => res.json({ message: 'Hello DEV.F' }));
 
